d0f9dd5cde5893c7c91c369ab7a22bf64123c700
RemcoGoy <remco.goy@hotmail.com>
2023-11-16 11:29:43 +0100

    First version of #2299

---
+++ solutions/2299/main.py
@@ -0,0 +1,50 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        SPECIAL_CHARS = "!@#$%^&*()-+"
+
+        n = len(password)
+        uppercase_flag = False
+        lowercase_flag = False
+        special_character_flag = False
+        digit_flag = False
+        adjacency_flag = False
+
+        if n < 8:
+            return False
+
+        prev_char = None
+        for i in range(n):
+            curr_char = password[i]
+            uppercase_flag = curr_char.isupper() or uppercase_flag
+            lowercase_flag = curr_char.islower() or lowercase_flag
+            digit_flag = curr_char.isdigit() or digit_flag
+            special_character_flag = (
+                curr_char in SPECIAL_CHARS or special_character_flag
+            )
+
+            if prev_char:
+                if prev_char == curr_char:
+                    adjacency_flag = True
+
+            prev_char = curr_char
+
+        return (
+            uppercase_flag
+            and lowercase_flag
+            and special_character_flag
+            and digit_flag
+            and not adjacency_flag
+        )
+
+
+solution = Solution()
+
+testcases = ["IloveLe3tcode!", "Me+You--IsMyDream", "1aB!"]
+outputs = [True, False, False]
+
+for index, testcase in enumerate(testcases):
+    response = solution.strongPasswordCheckerII(testcase)
+    if response == outputs[index]:
+        print(f"Case #{index}: Succeeded")
+    else:
+        print(f"Case #{index}: Failed")
---
+++ template.py
@@ -0,0 +1,16 @@
+class Solution:
+    def method(self):
+        pass
+
+
+solution = Solution()
+
+testcases = []  # Input
+outputs = []  # Output
+
+for index, testcase in enumerate(testcases):
+    response = solution.method()
+    if response in outputs[index]:
+        print(f"Case #{index}: Succeeded")
+    else:
+        print(f"Case #{index}: Failed")
