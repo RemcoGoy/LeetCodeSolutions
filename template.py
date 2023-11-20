33669787b1c574598b869f087707e8c27c893946
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2023-11-20 15:38:45 +0100

    Update template

--- template.py
+++ template.py
@@ -10,7 +10,7 @@ outputs = []  # Output
 
 for index, testcase in enumerate(testcases):
     response = solution.method()
-    if response in outputs[index]:
+    if response == outputs[index]:
         print(f"Case #{index}: Succeeded")
     else:
         print(f"Case #{index}: Failed")
