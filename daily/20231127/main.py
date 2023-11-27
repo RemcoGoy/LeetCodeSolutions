61e77d92610c700377bac971f866bddb318a2de2
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2023-11-27 07:02:38 +0100

    Daily

---
+++ daily/20231127/main.py
@@ -0,0 +1,44 @@
+from functools import lru_cache
+
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        @lru_cache(None)
+        def helper(n, row, col):
+            if n == 0:
+                return 1
+            ans = 0
+            for row_i, col_i in [
+                [2, 1],
+                [1, 2],
+                [-2, 1],
+                [-1, 2],
+                [2, -1],
+                [1, -2],
+                [-2, -1],
+                [-1, -2],
+            ]:
+                new_row = row + row_i
+                new_col = col + col_i
+                if (-1 < new_row < 3 and -1 < new_col < 3) or (new_row == 3 and new_col == 1):
+                    ans += helper(n - 1, new_row, new_col)
+            return ans % (10**9 + 7)
+
+        ans = helper(n - 1, 3, 1)
+        for row in range(3):
+            for col in range(3):
+                ans += helper(n - 1, row, col)
+        return ans % (10**9 + 7)
+
+
+solution = Solution()
+
+testcases = [1, 2]  # Input
+outputs = [10, 20]  # Output
+
+for index, testcase in enumerate(testcases):
+    response = solution.knightDialer(testcase)
+    if response == outputs[index]:
+        print(f"Case #{index}: Succeeded")
+    else:
+        print(f"Case #{index}: Failed")
